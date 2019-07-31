b3c8efa9abb2d18a951de93441c96b5c6fad183d
adamc66 <adam.cote66@gmail.com>
2019-07-31 15:24:39 -0400

    completed django relationships assignment 2, results in results.txt/Part 4 results.txt

---
+++ chinook/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+# Create your models here.
+
+class Artist(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Album(models.Model):
+    title = models.CharField(max_length=255, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name="albums")
+    
+    def __str__(self):
+        return self.title
+
+class MediaType(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    albums = models.ManyToManyField(Album, related_name="media_type")
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    media_types = models.ManyToManyField(MediaType,through='Track' ,related_name="genres")
+    albums = models.ManyToManyField(Album, through='Track', related_name="genres")
+
+class Track(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name="tracks")
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE, related_name="tracks")
+    media_type = models.ForeignKey(MediaType, on_delete=models.CASCADE, related_name="tracks")
+    name = models.CharField(max_length=255, null=False)
+    composer = models.CharField(max_length=255, null=True)
+    milliseconds = models.IntegerField(null=False)
+    bytes = models.IntegerField()
+    unit_price = models.DecimalField(decimal_places=2, max_digits=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Playlist(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    tracks = models.ManyToManyField(Track, related_name="playlists")
